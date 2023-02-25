f06c25035ce680936cfd7be2ea9e9daff1d9268c
rpf13 <rpf13@me.com>
2023-02-25 10:35:34 +0000

    Add templates to site title

--- run.py
+++ run.py
@@ -9,19 +9,22 @@ def index():
     return render_template("index.html")
 
 
+# The page_title variabl is used to send the content to the actual
+# frontend page on each of the sites, we have created a reference
+# to this jinja template. 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", page_title="About", list_of_numbers=[1, 2, 3])
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", page_title="Contact")
 
 
 @app.route("/careers")
 def careers():
-    return render_template("careers.html")
+    return render_template("careers.html", page_title="Careers")
 
 
 if __name__ == "__main__":
