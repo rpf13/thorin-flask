6757b8a0fa839302eb567e39b308f5a4c60ae9a7
rpf13 <rpf13@me.com>
2023-02-26 08:30:19 +0000

    Add member site and routing

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ def index():
 
 # The page_title variabl is used to send the content to the actual
 # frontend page on each of the sites, we have created a reference
-# to this jinja template. 
+# to this jinja template.
 @app.route("/about")
 def about():
     data = []
@@ -20,6 +20,19 @@ def about():
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
     return render_template("about.html", page_title="About", company=data)
+
+
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    # the first member in the arguments stands for the member.html and the
+    # second member tands for the member var we have created in this method
+    return render_template("member.html", member=member)
 
 
 @app.route("/contact")
