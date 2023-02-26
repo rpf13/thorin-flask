a7e6487a0789cadb2430f4a5b85f0146cfe893ac
rpf13 <rpf13@me.com>
2023-02-26 10:47:55 +0000

    Add form & routing

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 import json
+if os.path.exists("env.py"):
+    import env
+
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
@@ -35,8 +39,11 @@ def about_member(member_name):
     return render_template("member.html", member=member)
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
